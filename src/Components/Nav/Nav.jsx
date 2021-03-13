36c4451911786143aca699c79fa37ae2e3855916
masonleonhart <mason.leonhart@gmail.com>
2021-03-13 17:41:30 -0600

    built nav drawer

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,46 +1,85 @@
+import { useState } from 'react';
 import { Link } from 'react-scroll';
 
 import './Nav.css';
-import { AppBar, Toolbar, Button, makeStyles, useMediaQuery } from '@material-ui/core';
+import { AppBar, Toolbar, Button, makeStyles, useMediaQuery, Drawer } from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/Menu';
 
 function Nav() {
-    const useStyles = makeStyles({
+    const [drawerToggle, setDrawerToggle] = useState(false);
+
+    const useStyles = makeStyles((theme) => ({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            position: 'sticky'
+        },
         navToolbar: {
             minHeight: 'fit-content'
         },
         navButton: {
             fontWeight: 'bold'
+        },
+        drawer: {
+            width: '40%'
+        },
+        drawerPaper: {
+            width: '40%'
         }
-    });
+    }));
 
     const classes = useStyles();
 
     const matches = useMediaQuery('(min-width: 1000px)');
 
     return (
-        <AppBar position='sticky'>
-            <Toolbar id='nav' className={classes.navToolbar}>
-                {matches ? <div id='nav-links'>
-                    <Button className={classes.navButton} color='secondary'>
-                        <Link to='me' activeClass='active' spy={true} smooth={true} duration={500}>Mason</Link>
+        <>
+            <AppBar className={classes.appBar}>
+                <Toolbar id='nav' className={classes.navToolbar}>
+                    {matches ? <div id='nav-links'>
+                        <Button className={classes.navButton} color='secondary'>
+                            <Link to='me' activeClass='active' spy={true} smooth={true} duration={500}>Mason</Link>
+                        </Button>
+                        <Button className={classes.navButton} color='secondary'>
+                            <Link to='about' activeClass='active' spy={true} smooth={true} duration={500}>About</Link>
+                        </Button>
+                        <Button className={classes.navButton} color='secondary'>
+                            <Link to='portfolio' activeClass='active' spy={true} smooth={true} duration={500}>Portfolio</Link>
+                        </Button>
+                        <Button className={classes.navButton} color='secondary'>
+                            <Link to='education' activeClass='active' spy={true} smooth={true} duration={500}>Education</Link>
+                        </Button>
+                    </div> :
+                        <Button
+                            color='secondary'
+                            onClick={() => !drawerToggle ? setDrawerToggle(true) : setDrawerToggle(false)}
+                        >
+                            <MenuIcon />
+                        </Button>}
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                open={drawerToggle}
+                onClose={() => setDrawerToggle(false)}
+                variant='persistent'
+            >
+                <Toolbar />
+                <div id='drawer-links'>
+                    <Button className={classes.navButton} color='primary'>
+                        <Link to='me' activeClass='drawer-active' spy={true} smooth={true} duration={500}>Mason</Link>
                     </Button>
-                    <Button className={classes.navButton} color='secondary'>
-                        <Link to='about' activeClass='active' spy={true} smooth={true} duration={500}>About</Link>
+                    <Button className={classes.navButton} color='primary'>
+                        <Link to='about' activeClass='drawer-active' spy={true} smooth={true} duration={500}>About</Link>
                     </Button>
-                    <Button className={classes.navButton} color='secondary'>
-                        <Link to='portfolio' activeClass='active' spy={true} smooth={true} duration={500}>Portfolio</Link>
+                    <Button className={classes.navButton} color='primary'>
+                        <Link to='portfolio' activeClass='drawer-active' spy={true} smooth={true} duration={500}>Portfolio</Link>
                     </Button>
-                    <Button className={classes.navButton} color='secondary'>
-                        <Link to='education' activeClass='active' spy={true} smooth={true} duration={500}>Education</Link>
+                    <Button className={classes.navButton} color='primary'>
+                        <Link to='education' activeClass='drawer-active' spy={true} smooth={true} duration={500}>Education</Link>
                     </Button>
-                </div> :
-                    <Button color='secondary'>
-                        <MenuIcon />
-                    </Button>}
-            </Toolbar>
-        </AppBar>
+                </div>
+            </Drawer>
+        </>
     );
 };
 
